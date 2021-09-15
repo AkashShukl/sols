66a263a8b903eaab8d579e2be86e261a029a7de4
shuklaa884 <shuklaa884@gmail.com>
2021-09-15 16:38:41 +0530

    Solution 3

--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -5,11 +5,11 @@ export default function ListView(props) {
   let endValue = props.end ? Number(props.end) : 10;
 
   const genList = () => {
-    let items = []
-    for(let i=startValue;i<= endValue;i++){
-        items.push(i)
+    let items = [];
+    for (let i = startValue; i <= endValue; i++) {
+      items.push(i);
     }
-    return items.map((index)=>( <li key={index}> {index} </li>))
+    return items.map((index) => <li key={index}> {index} </li>);
   };
   return (
     <div>
