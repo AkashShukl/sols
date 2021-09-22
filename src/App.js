9134225482d0cca440efa7f87cd8f6871c72468d
shuklaa884 <shuklaa884@gmail.com>
2021-09-22 13:58:39 +0530

    solution-6 react-context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
-
+import React, { useState } from "react";
 import Parent from "./components/Parent";
 
+export const ToggleUserNameContext = React.createContext({});
+
 function App() {
+  const [toggleSwitch, settoggleSwitch] = useState(false);
+  const handleToggle = () => {
+    settoggleSwitch(!toggleSwitch);
+  };
   return (
     <div className="app">
       <h1> Exercise 5</h1> <br />
-      <Parent counterStartVal={ 5 } />
+      <ToggleUserNameContext.Provider value={{ toggleSwitch, handleToggle }}>
+        <Parent counterStartVal={5} />
+      </ToggleUserNameContext.Provider>
       <hr />
     </div>
   );
--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,48 +1,66 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ToggleUserNameContext } from "../App";
 
 export default function SignupForm(props) {
- 
+  const toggleSwitch = useContext(ToggleUserNameContext);
   return (
     <div>
-    <div>
-    <label>Name</label>
-    <input type="text" value={props.name} onChange={(e) => props.handleNameChange(e)}></input>
-    {props.name ? " " : <label style={{ color: "red" }}> *required </label>}
-    <br />
-    <label>Email</label>
-    <input type="text" value={props.email} onChange={(e) => props.handleEmailChange(e)}></input>
-    {props.email.match(/.*@.*\..*/) ? " " : <label style={{ color: "red" }}> *Invalid Email </label>}
-    <br />
-    <label>Password</label>
-    <input
-      type="password"
-      value={props.password}
-      onChange={(e) => props.handlePasswordChange(e)}
-    ></input>{" "}
-    {props.password ? " " : <label style={{ color: "red" }}> *required </label>}
-    <br />
-    <label>Confirm Password</label>
-    <input
-      type="password"
-      value={props.confirmPassword}
-      onChange={(e) => props.handleConfirmPasswordChange(e)}
-    ></input>{" "}
-    {props.confirmPassword ? (
-      " "
-    ) : (
-      <label style={{ color: "red" }}> *required </label>
-    )}
-    <br />
-    {props.password === props.confirmPassword ? (
-      " "
-    ) : (
-      <label style={{ color: "red" }}> Passwords do not match </label>
-    )}
-    <br />
-    <button type="submit" onClick={() => props.handleSubmit()}>
-      Submit
-    </button>
-  </div>
+      <div>
+        <label hidden={toggleSwitch.toggleSwitch}>Name</label>
+        <input
+          hidden={toggleSwitch.toggleSwitch}
+          type="text"
+          value={props.name}
+          onChange={(e) => props.handleNameChange(e)}
+        ></input>
+        {props.name ? " " : <label hidden={toggleSwitch.toggleSwitch} style={{ color: "red" }}> *required </label>}
+        <br />
+        <label>Email</label>
+        <input
+          type="text"
+          value={props.email}
+          onChange={(e) => props.handleEmailChange(e)}
+        ></input>
+        {props.email.match(/.*@.*\..*/) ? (
+          " "
+        ) : (
+          <label style={{ color: "red" }}> *Invalid Email </label>
+        )}
+        <br />
+        <label>Password</label>
+        <input
+          type="password"
+          value={props.password}
+          onChange={(e) => props.handlePasswordChange(e)}
+        ></input>{" "}
+        {props.password ? (
+          " "
+        ) : (
+          <label style={{ color: "red" }}> *required </label>
+        )}
+        <br />
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          value={props.confirmPassword}
+          onChange={(e) => props.handleConfirmPasswordChange(e)}
+        ></input>{" "}
+        {props.confirmPassword ? (
+          " "
+        ) : (
+          <label style={{ color: "red" }}> *required </label>
+        )}
+        <br />
+        {props.password === props.confirmPassword ? (
+          " "
+        ) : (
+          <label style={{ color: "red" }}> Passwords do not match </label>
+        )}
+        <br />
+        <button type="submit" onClick={() => props.handleSubmit()}>
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
