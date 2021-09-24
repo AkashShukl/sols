a6d7ac73da866c75e9d2d6f7aa76b4864353f9fe
shuklaa884 <shuklaa884@gmail.com>
2021-09-24 10:57:53 +0530

    solution-9 redux

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { useParams } from "react-router";
 import Greetings from "./Greetings";
+import { useSelector } from "react-redux";
 
 export default function Home() {
   let params = useParams();
-
+  const { userInfo }= useSelector(state => state.formreducer)
   return (
     <div>
       <h3>
         < Greetings name = {params.username ? params.username : "User" } />
+        {userInfo.name} {userInfo.email} {userInfo.password} {userInfo.confirmPassword}
       </h3>
     </div>
   );
